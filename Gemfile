a7072870203ca09de3fa6fa9be58e0a48aec9acf
thannes30 <thannes30@yahoo.com>
2016-02-29 11:00:08 -0500

    ssh key fixed for personal computer

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'sinatra'
+gem 'sinatra', '1.1.0'
 gem 'toml'
+
+ruby "2.1.7"
--- inthepub.rb
+++ inthepub.rb
@@ -7,13 +7,48 @@ require "date"
 require "toml"
 require "tilt/erb"
 
-
 get '/' do
-  is_he_there = get_untappd_data
-  erb is_he_there
+  @beers = beer_search
+
+  erb :home
 end
 
-def get_untappd_data
+get '/tim' do
+  is_he_there = tims_drinking
+
+  erb :beer
+end
+
+def beer_search(beername = "coors")
+
+  if !@config
+    @config  = TOML.load_file('config.toml')['untappd']
+    raise 'Could not load credentials file at config.toml' if @config.nil? || @config.empty?
+  end
+  uri = URI.parse("https://api.untappd.com/v4/user/checkins/#{beername}\?client_id\=#{@config['client_id']}\&client_secret\=#{@config['client_secret']}")
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+  request = Net::HTTP::Get.new(uri.request_uri)
+
+  response = http.request(request)
+  json =  JSON.parse(response.body)
+
+  @beers = []
+
+  beers_array = json["response"]["beers"]["items"]
+
+  beers_array.each do |beer|
+    beer = beer["beer"]
+    @beers << beer
+  end
+
+  @beers
+end
+
+
+def tims_drinking
   if !@config
     @config  = TOML.load_file('config.toml')['untappd']
     raise 'Could not load credentials file at config.toml' if @config.nil? || @config.empty?
@@ -28,20 +63,18 @@ def get_untappd_data
   response = http.request(request)
   json =  JSON.parse(response.body)
 
-  if (DateTime.parse(json["response"]["checkins"]["items"][0]["created_at"]).to_time.to_i) > (Time.now - 3600).to_i
-    if json["response"]['checkins']['items'][0]['venue'] == []
-      return "Nope, but he is drinking!"
-    else
-      return "Yup!"
-    end
-  elsif (DateTime.parse(json["response"]["checkins"]["items"][0]["created_at"]).to_time.to_i) > (Time.now - 21600).to_i
-    if json["response"]['checkins']['items'][0]['venue'] == nil
-      return "Nope, but he was drinking!"
-    else
-      return "Possibly..."
-    end
-  else
-    return "Nope! :("
-end
+  @beers = []
 
+  # @beer_style = json["response"]["checkins"]["items"][1]["beer"]["beer_style"]
+  # @beer_abv = json["response"]["checkins"]["items"][0]["beer"]["beer_abv"]
+  #@brewery = json["response"]["checkins"]["items"][2]["brewery"]["brewery_name"]
+
+  beers_array = json["response"]["checkins"]["items"]
+
+  beers_array.each do |beer|
+    beer = beer["beer"]
+    @beers << beer
+  end
+
+  @beers
 end
